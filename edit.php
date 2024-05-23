42453f4a24a7d444358247d3bce9c81c52f23bdc
Pedro Henrique Dantas <40534089+dantas-726@users.noreply.github.com>
2024-05-23 16:57:35 -0300

    Initial commit

---
+++ edit.php
@@ -0,0 +1,67 @@
+<?php
+// This file is part of Moodle Course Rollover Plugin
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package     local_message
+ * @author      Pedro
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+use local_message\form\edit;
+use local_message\manager;
+
+require_once(__DIR__ . '/../../config.php');
+
+global $DB;
+
+$PAGE->set_url(new moodle_url('/local/message/edit.php'));
+$PAGE->set_context(\context_system::instance());
+$PAGE->set_title(get_string('Editar'));
+
+
+// We want to display our form.
+$mform = new edit();
+
+
+// Form processing and displaying is done here.
+
+if ($mform->is_cancelled()) {
+ // Go back to the manage.php page 
+   redirect($CFG->wwwroot . '/local/message/manage.php', 'Você cancelou a mensagem');
+} else if ($fromform = $mform->get_data()) {
+  // When the form is submitted, and the data is successfully validated,
+  // the `get_data()` function will return the data posted in the form.
+
+  // Insert the data into our database table
+
+  $recordinsert = new stdClass();
+  $recordinsert->messagetext = $fromform->messagetext;
+  $recordinsert->messagetype = $fromform->messagetype;
+
+  $DB->insert_record('local_message', $recordinsert);
+
+  // Go back to manage.php 
+
+  redirect($CFG->wwwroot. '/local/message/manage.php', 'Você criou a mensagem: '. $fromform->messagetext);
+
+} 
+
+echo $OUTPUT->header();
+
+$mform->display();
+
+echo $OUTPUT->footer();
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Local message
+This is a notifications plugin for moodle. It will display notifications to users and allows admins to create them.
+
+
+
+# Functionality
+- Form for admins to add new notification
+- Store read messages for users, do not display twice.
+- Store notifications in database.
+- Unit testing for message display behaviour
+- External function to delete messages
+- Modal popup to confirm deletion
